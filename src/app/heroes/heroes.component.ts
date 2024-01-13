c8b6ac61321f1409b60965954c1da670807817ec
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-13 11:59:46 +0100

    restructured the data  and added hero details comp

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
@@ -15,4 +17,12 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero?: Hero;
+
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero =  hero;
+  }
 }
