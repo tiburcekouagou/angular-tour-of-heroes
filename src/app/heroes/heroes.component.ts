ea007a30d4c4807fbd9b5b6d40abbb93fd015c86
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-13 12:39:29 +0100

    added message service to components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -23,21 +24,25 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+    ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
-  
+
   getHeroes(): void {
     // this.heroes = this.heroService.getHeroes();
     this.heroService
-    .getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
-    this.selectedHero =  hero;
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
-  message: string[] = [];
+  messages: string[] = [];
 
   add(message: string) {
-    this.message.push(message);
+    this.messages.push(message);
   }
 
   clear() {
-    this.message = [];
+    this.messages = [];
   }
 }
